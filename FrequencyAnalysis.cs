c362b43d71fbf697ef90e8b1137c558bb96936f1
Jamison Macfarland <JMacfarland@carbonite.com>
2017-09-21 16:10:01 -0400

    First commit

---
+++ FrequencyAnalysis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crypto
+{
+    class FrequencyAnalysis
+    {
+        private static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static int[] getNumLetterOccurrences(string text)
+        {
+            int[] frequency = new int[26];
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                for(int n = 0; n < alphabet.Length; n++)
+                {
+                    if(text[i] == alphabet[n])
+                    {
+                        frequency[n]++;
+                    }
+                }
+            }
+
+            return frequency;
+        }
+        public static int[] printNumLetterOccurrences(string text)
+        {
+            int[] frequency = getNumLetterOccurrences(text);
+
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(alphabet[i] + ":" + frequency[i] + ", ");
+            }
+
+            return frequency;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace crypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubstitutionCipher sc = new SubstitutionCipher();
+
+            string plainText = "This is a test string. There are many like it, but this one is mine.";
+            string cipherText = sc.encode(plainText);
+            string newPlain = sc.decode(cipherText);
+
+            var frequency = FrequencyAnalysis.printNumLetterOccurrences(cipherText);
+        }
+    }
+}
