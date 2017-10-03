a0c8c1c22ef711c6d4e5abf9861fc5aa318ef941
Jamison Macfarland <JMacfarland@carbonite.com>
2017-10-03 15:13:14 -0400

    Added code to deal with MS "smart" quotes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace crypto
         static void Main(string[] args)
         {
             SubstitutionCipher sb = new SubstitutionCipher();
-            string plainText = "Mother died today. Or maybe yesterday, I don’t know. I had a telegram from the home: ‘Mother passed away. Funeral tomorrow. Yours sincerely.’ That doesn’t mean anything. It may have been yesterday.";
+            string plainText = "Mother died today. Or maybe yesterday, I don't know. I had a telegram from the home: ‘Mother passed away. Funeral tomorrow. Yours sincerely.' That doesn’t mean anything. It may have been yesterday.";
+            plainText = plainText.Replace('’', '\'').Replace('‘', '\'');
             string key = sb.generateNewKey();
             string cipherText = sb.encode(plainText, key);
             string newPlain = sb.decode(cipherText, key);
